41f6e61859caf3317793645199dce013cdc612c8
Readul Hasan Chayan [Heemayl] <heemayl@gmail.com>
2023-06-28 13:26:23 +0600

    Add explanation for `asyncio.sleep(0)`

--- chat/consumers.py
+++ chat/consumers.py
@@ -19,11 +19,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
 
-    async def disconnect(self, close_code):
-        pass
-
-    async def receive(self, text_data):
+        # When a websocket client connects, accept the connection
+        # and get the data from `_get_content_async` as it comes
+        # (and also send it to the client via the websocket)
         while True:
             content = await _get_content_async()
+
+            # The `asyncio.sleep(0)` is needed for context-switching i.e.
+            # to send the control back to the event loop so that it
+            # can schedule some other async task in the meantime.
+
+            # IMPORTANT: Without `asyncio.sleep(0)`, no other task can
+            # be scheduled by the event loop so the `self.send` in the
+            # following line won't even run i.e. no data could be sent
+            # back via the websocket as it won't get a chance to run.
+
+            # We can use any `sleep` value apart from 0 depending on
+            # context but 0 is enough if our only intention is to do
+            # context switching; so `asyncio.sleep(0)` is essentially
+            # a trick when doing asyncio.
             await asyncio.sleep(0)
+
             await self.send(text_data=json.dumps({"message": content}))
+
+    async def disconnect(self, close_code):
+        pass
